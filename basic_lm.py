ebde0b98598b1ef2842a7ff9c44fbfbf9a6f2f9b
Benjamin Anderson <andersonbcdefg@gmail.com>
2025-04-12 19:43:39 -0700

    compile

--- basic_lm.py
+++ basic_lm.py
@@ -87,6 +87,7 @@ def train(config: Config | None = None):
         config.num_heads,
         config.num_layers
     ).to(device)
+    model.forward = torch.compile(model.forward)
     optimizer = torch.optim.AdamW(model.parameters(), lr=config.learning_rate)
     scheduler = torch.optim.lr_scheduler.LambdaLR(
         optimizer, lambda step: step / config.warmup_steps if step < config.warmup_steps else (config.total_steps - step) / (config.total_steps - config.warmup_steps)
@@ -99,7 +100,10 @@ def train(config: Config | None = None):
             for inputs, targets in data_loader(config.batch_size, config.seq_len):
                 with torch.autocast(device_type=device, enabled=device=="cuda"):
                     logits = model(inputs.to(device))
-                loss = F.cross_entropy(logits.view(-1, logits.shape[-1]), targets.reshape(-1).to(device))
+                loss = F.cross_entropy(
+                    logits.view(-1, logits.shape[-1]),
+                    targets.reshape(-1).to(device)
+                )
                 scaler.scale(loss).backward()
                 scaler.step(optimizer)
                 scaler.update()
